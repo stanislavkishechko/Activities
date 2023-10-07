e21dfc4053084fa57356a643a7d41c14607470d3
Stanislav Kishechko <stanislavkishechko@gmail.com>
2023-10-08 00:21:21 +0300

    I was styling the user interface

---
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -0,0 +1,46 @@
+import { Button, Icon, Item, Segment, SegmentGroup } from "semantic-ui-react";
+import { Activity } from "../../../app/models/activity";
+import { Link } from "react-router-dom";
+
+interface Props {
+    activity: Activity
+}
+
+export default function ActivityListItem({ activity }: Props) {
+    return (
+        <SegmentGroup>
+            <Segment>
+                <Item.Group>
+                    <Item>
+                        <Item.Image size='tiny' circular src='/assets/user.png' />
+                        <Item.Content>
+                            <Item.Header as={Link} to={`/activities/${activity.id}`}>
+                                {activity.title}
+                            </Item.Header>
+                        </Item.Content>
+                        <Item.Description>Hosted by Stanislav</Item.Description>
+                    </Item>
+                </Item.Group>
+            </Segment>
+            <Segment>
+                <span>
+                    <Icon name='clock' /> {activity.date}
+                    <Icon name='marker' /> {activity.venue}
+                </span>
+            </Segment>
+            <Segment>
+                Attendees go here
+            </Segment>
+            <Segment clearing>
+                <span>{activity.description}</span>
+                <Button
+                    as={Link}
+                    to={`/activities/${activity.id}`}
+                    color='teal'
+                    floated='right'
+                    content='View' 
+                />
+            </Segment>
+        </SegmentGroup>
+    )
+}
