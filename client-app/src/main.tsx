6e581c90f6a96da367142645dcda26239d30b761
Stanislav Kishechko <stanislavkishechko@gmail.com>
2023-10-10 17:52:25 +0300

    Added form validation

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -4,6 +4,7 @@ import 'semantic-ui-css/semantic.min.css';
 import 'react-calendar/dist/Calendar.css';
 import 'react-toastify/dist/ReactToastify.min.css'
 import './app/layout/styles.css';
+import 'react-datepicker/dist/react-datepicker.css'
 import { StoreContext, store } from './stores/store';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './app/router/Routes';
